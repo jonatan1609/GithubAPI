f501e5d5e80aa4e813ecdbc92154eac52a4a0e24
Jonathan <5377074j@gmail.com>
2021-09-09 23:48:49 +0300

    Add: delete_post

---
+++ github/gists/delete_post.py
@@ -0,0 +1,18 @@
+from ..utils import Delete
+
+
+def delete_post(
+        token: str,
+        gist_id: str,
+        accept: str = "application/vnd.github.v3+json"
+) -> None:
+    """
+    Delete a gist.
+    :param token: Your github token to perform actions.
+    :param gist_id: The ID of the gist.
+    :param accept: Default: "application/vnd.github.v3+json"
+    :return: None
+    """
+    url = f"https://api.github.com/gists/{gist_id}"
+    with Delete(url=url, headers={"Accept": accept}, auth=("token", token)) as response:
+        return response
--- github/utils.py
+++ github/utils.py
@@ -4,9 +4,16 @@ from requests import get, post, patch, delete, put
 
 class Request:
     response = ...
+    is_json = True
 
     def __class_getitem__(cls, item):
-        return cls(item)
+        if isinstance(item, tuple):
+            method, json = item
+            self = cls(method)
+            self.is_json = json
+        else:
+            self = cls(item)
+        return self
 
     def __init__(self, method: typing.Callable):
         self.method = method
@@ -21,9 +28,13 @@ class Request:
     def __enter__(self):
         if self.response is Ellipsis:
             self.evaluate()
-        if self.response.status_code not in {200, 201}:
-            raise RuntimeError(self.response.json()["message"])
-        return self.response.json()
+        self.response.raise_for_status()
+        if self.response.text.strip():
+            if self.response.status_code not in {200, 201}:
+                raise RuntimeError(self.response.json()["message"])
+            if self.is_json:
+                return self.response.json()
+            return self.response.text
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
@@ -32,5 +43,5 @@ class Request:
 Get = Request[get]
 Post = Request[post]
 Patch = Request[patch]
-Delete = Request[delete]
+Delete = Request[delete, False]
 Put = Request[put]
