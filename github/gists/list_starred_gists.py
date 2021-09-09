a5c58e75d28182bb115f2f23cbd762d2119e03fd
Jonathan <5377074j@gmail.com>
2021-09-09 22:29:44 +0300

    Add: list_starred_gists

---
+++ github/gists/list_starred_gists.py
@@ -0,0 +1,33 @@
+from ..utils import Get
+
+
+def list_starred_gists(
+        token: str,
+        since: str = None,
+        per_page: int = 30,
+        page: int = 1,
+        accept: str = "application/vnd.github.v3+json"
+):
+    """
+    Lists the authenticated user's starred gists.
+    :param token: User authentication.
+    :param since: Only show notifications updated after the given time. This is a timestamp in ISO 8601 format: YYYY-MM-DDTHH:MM:SSZ.
+    Default: forever.
+    :param per_page: Results per page (max 100). Default: 30
+    :param page: Page number of the results to fetch. Default: 1
+    :param accept: Setting to application/vnd.github.v3+json is recommended.
+    :return: `dict`
+    :raises: `RuntimeError` in case one of the parameters is invalid.
+
+    **Important: In order to retrieve Secret Gists too, you must not user a regular OAuth Token
+    but a special token which is being used in a GitHub App.**
+    """
+    url = "https://api.github.com/gists/starred"
+
+    with Get(
+            url,
+            headers={"Accept": accept},
+            params={"since": since, "per_page": per_page, "page": page},
+            auth=("token", token)
+    ) as response:
+        return response
