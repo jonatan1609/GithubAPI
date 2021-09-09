50eabbd085437939cd0e2e57fc2941cdfea76c55
Jonathan <5377074j@gmail.com>
2021-09-09 23:24:35 +0300

    Enhance: Add a particular type for gist which happens in list_*_gists

--- github/gists/__init__.py
+++ github/gists/__init__.py
@@ -1,6 +1,6 @@
 from .post_gist import post_gist
 from .get_gist import get_gist
-from .data_types import File, Gist, User, History, ChangeStatus, Fork, ForkUser
+from .data_types import File, Gist, User, History, ChangeStatus, Fork, ForkUser, SearchResultGist
 from .list_all_gists import list_all_gists
 from .list_starred_gists import list_starred_gists
 from .update_gist import update_gist
