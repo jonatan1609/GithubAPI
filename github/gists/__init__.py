15b8d402c19f90e6bc2ead6d2c28ffb9d44cc45f
Jonathan <5377074j@gmail.com>
2021-09-09 22:12:42 +0300

    Enhance: Rename list_gists to list_all_gists

--- github/gists/__init__.py
+++ github/gists/__init__.py
@@ -1,4 +1,5 @@
 from .post_gist import post_gist
 from .get_gist import get_gist
 from .data_types import File, Gist, User, History, ChangeStatus, Fork, ForkUser
-from .list_gists import list_all_gists
+from .list_all_gists import list_all_gists
+
