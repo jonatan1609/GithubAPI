68259e175b0e19478c46d3a9d62682025f83c571
Jonathan <5377074j@gmail.com>
2021-09-24 14:26:29 +0300

    Fix: fix response handling when deleting gists
    Also, fixed documentation

--- github/gists/delete_gist.py
+++ github/gists/delete_gist.py
@@ -5,14 +5,18 @@ def delete_gist(
         token: str,
         gist_id: str,
         accept: str = "application/vnd.github.v3+json"
-) -> None:
+) -> bool:
     """
     Delete a gist.
     :param token: Your github token to perform actions.
     :param gist_id: The ID of the gist.
     :param accept: Default: "application/vnd.github.v3+json"
-    :return: None
+    :return: `bool` - True in case the gist was deleted successfully.
     """
     url = f"https://api.github.com/gists/{gist_id}"
-    with Delete(url=url, headers={"Accept": accept}, auth=("token", token)) as response:
-        return response
+    request = Delete(url=url, headers={"Accept": accept}, auth=("token", token))
+
+    request.evaluate()
+    request.response.raise_for_status()
+    if request.response.status_code == 204:
+        return True
